a739cadb3fa3fadc9d26ee6c40fb936b3a866ebe
SudoYummy <mattwprice29@gmail.com>
2022-09-17 16:49:11 -0400

    powershell files

---
+++ powershell/price3mw-lab3.ps1
@@ -0,0 +1,3 @@
+﻿$BODY = "This machine's IP is " + ((Get-NetIPAddress).IPv4Address | Select-String "192*") + ". User is " + [System.Environment]::UserName + ". Hostname is " + (hostname) + ". PowerShell Version" + $HOST.Version.Major + ". Today's Date is " + (Get-Date -Format "dddd, MM dd yyyy")
+
+$BODY | Out-File ~\machineinfo.txt
