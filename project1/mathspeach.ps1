d0ddc27b9057b2eff08b4798b64651de1103aa5d
SudoYummy <mattwprice29@gmail.com>
2022-10-08 22:23:27 -0400

    added powershell script

---
+++ project1/mathspeach.ps1
@@ -0,0 +1,14 @@
+﻿##Source: www.pdq.com/blog/powershell-text-to-speech-examples/
+
+Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+$speak.Speak("You will be adding two numbers")
+Write-Host "You will be adding two numbers together"
+
+$num1 = Read-Host -Prompt "Enter first number"
+$num2 = Read-Host -Prompt "Enter second number"
+$answer = [int]$num1 + [int]$num2
+
+$speak.Speak("$num1 + $num2 = $answer")
+Write-Host("$num1 + $num2 = $answer")
