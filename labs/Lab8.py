a39eb05677cb1bab775069f58a5f2abbd025a773
SudoYummy <mattwprice29@gmail.com>
2022-10-30 20:55:50 -0400

    adding lab 8

---
+++ labs/Lab8.py
@@ -0,0 +1,19 @@
+from urllib import request
+from bs4 import BeautifulSoup
+import requests, re
+
+data = requests.get('http://www.espn.com/nfl/college/_/letter/o').content
+output = []
+soup = BeautifulSoup(data, 'html.parser')
+
+table = soup.findAll("tr", {"class":re.compile('(evenrow player-\d\d-\d\d\d\d\d\d\d|oddrow player-\d\d-\d\d\d\d\d\d\d)')})
+
+for player in table:
+    name = player.findAll("a", {"href":re.compile('(http:\/\/www\.espn\.com\/nfl\/player\/_\/id\/\d\d\d\d\d\d\d\/.+)')})
+    
+    for n in name:
+        output.append((re.compile('>(.+)<').findall(str(n))))
+        
+
+for entry in output:
+    print(entry)
