fa548af157bf8ff274f8349efe4d40fcce9b919d
SudoYummy <mattwprice29@gmail.com>
2022-11-16 19:00:37 -0500

    adding sources

--- project3/project3.py
+++ project3/project3.py
@@ -1,5 +1,10 @@
 import requests
 import config
+
+#Sources
+##How to hide API Key: https://medium.com/black-tech-diva/hide-your-api-keys-7635e181a06c
+##VirusTotal API docs: https://developers.virustotal.com/reference/ip-info
+
 url = "https://www.virustotal.com/api/v3/ip_addresses/18.4.56.78"
 
 headers = {
