2aa8f1b27142aa1e204792bd57d675f87e6f7db3
SudoYummy <mattwprice29@gmail.com>
2022-11-21 09:27:16 -0500

    adding web.py

--- project3/project3.py
+++ project3/project3.py
@@ -1,9 +1,13 @@
 import requests
 import config
-
+from flask import Flask
+from routes import app
 #Sources
 ##How to hide API Key: https://medium.com/black-tech-diva/hide-your-api-keys-7635e181a06c
 ##VirusTotal API docs: https://developers.virustotal.com/reference/ip-info
+
+app = Flask(__name__)
+app.config.from_object(__name__)
 
 url = "https://www.virustotal.com/api/v3/ip_addresses/18.4.56.78"
 
@@ -16,4 +20,6 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
-print(response.text)
+@app.route('/')
+def test():
+    return response.text
