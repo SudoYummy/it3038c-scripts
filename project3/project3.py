34ff2483a39d26f078fbc2bb3c1a28138d7969f8
SudoYummy <mattwprice29@gmail.com>
2022-11-28 11:11:05 -0500

    added website pages and error checking mechanisms

--- project3/project3.py
+++ project3/project3.py
@@ -1,24 +1,48 @@
 import requests
 import config
-from flask import Flask
+from flask import Flask, render_template, request
+import ipaddress
 #Sources
 ##How to hide API Key: https://medium.com/black-tech-diva/hide-your-api-keys-7635e181a06c
 ##VirusTotal API docs: https://developers.virustotal.com/reference/ip-info
+##How to use ipaddess module: https://docs.python.org/3/library/ipaddress.html
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-url = "https://www.virustotal.com/api/v3/ip_addresses/18.4.56.78"
-
-headers = {
-    "accept": "application/json",
-
-    #If you wish to use your own API key replace below line with" "x-apikey": "yourfullapikeyfromVT"
-    "x-apikey": config.api_key
-}
-
-response = requests.get(url, headers=headers)
-
 @app.route('/')
-def test():
-    return response.text
+def home():
+    return render_template("index.html")
+    
+@app.route('/results', methods=['POST'])
+def results():
+    #This makes the request to VirusTotal. The entered ipaddress is used for the URL
+    #ipvalue holds the entered IP address
+    ipvalue=request.form['data']
+    #making sure value entered was IP address
+    def ipv4(string):
+        try:
+            ipaddress.IPv4Network(string)
+            return True
+        except ValueError:
+            return False
+    if ipv4(ipvalue) == False:
+        return "This is not a valid IP address please return to home page"
+
+    #Below code will run only if the IP is valid
+    url = "https://www.virustotal.com/api/v3/ip_addresses/%s" % (ipvalue)
+    headers = {
+        "accept": "application/json",
+        #If you wish to use your own API key replace below line with" "x-apikey": "yourfullapikeyfromVT"
+        "x-apikey": config.api_key
+    }
+    response = requests.get(url, headers=headers)
+    #jsondata holds the response from VT
+    ##'tags' attribute determines whether or not it is private
+    jsondata = response.json()
+    #Determine if the IP address is private/public
+    try:
+        if jsondata['data']['attributes']['tags'][0] == 'private':
+            return "This is a private IP address. This cannot be searched with VirusTotal. Please return to homepage and enter a public IP."
+    except:
+        return render_template("results.html", submission=jsondata)
